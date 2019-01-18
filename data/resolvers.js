2ceea92c1021de5a435f533c234058631fc4812d
sasha <sasha@guesty.com>
2019-01-18 12:08:09 +0200

    6. Connecting to MongoDB

---
+++ data/resolvers.js
@@ -0,0 +1,25 @@
+import { Author, View } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(_, args) {
+      return Author.find({ where: args });
+    },
+  },
+  Author: {
+    posts(author) {
+      return author.getPosts();
+    },
+  },
+  Post: {
+    author(post) {
+      return post.getAuthor();
+    },
+    views(post) {
+      return View.findOne({ postId: post.id })
+        .then((view) => view.views);
+    },
+  },
+};
+
+export default resolvers;
